b8215170e4a040fe6dade98016c4c4fd318691a6
Alejorom09 <67286835+Alejorom09@users.noreply.github.com>
2020-06-24 13:59:48 -0500

    Cambio homo

--- app/src/main/java/com/example/test4/MainActivity.kt
+++ app/src/main/java/com/example/test4/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //hola
+
+        //como estás?
+
+        //yo muy bien y tú?
+
+        //bien, siendo un maldito helicoptero de ocho helices con motor triple velocidad
     }
 }
