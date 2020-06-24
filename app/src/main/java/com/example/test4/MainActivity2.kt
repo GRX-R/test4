45d36edacacff66a4af27748d5b429e6c21ba490
GRX-R <grx1239@gmial.com>
2020-06-24 14:46:31 -0500

    Cambio No.1

--- app/src/main/java/com/example/test4/MainActivity2.kt
+++ app/src/main/java/com/example/test4/MainActivity2.kt
@@ -8,10 +8,37 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        //hola
+        primerFuncion()
 
-        //como estas?
+    }
 
-        //PerroGato
+    private fun primerFuncion(){
+
+        // Empieza una fuesta...
+        //Es una fiesta de mascotas.
+        //Un sapo hp le da por llevar 126 perros...
+
+        val perros = 126
+
+        //Ahora, resulta que la tia carmensa se trajo a sus 48 gatos, mucha sapaperra
+
+        val gatos = 48
+
+        //Y, a tu primo con down, le dio por trar sus camaleones. 3 camaleones.
+
+        val camaleones = 3
+
+        //¿Cuántos animales hay en la fiesta?
+
+        val animales = perros + gatos + camaleones
+
+        //¿Cuántos de estos animales son mamíferos?
+
+        val mamiferos = perros + gatos
+
+        //Mostrar en pantalla
+
+        println("En total, tu extraña familia y amigos, trajeron... " + animales + " animales, y hay " + mamiferos + " mamíferos.")
+
     }
 }
