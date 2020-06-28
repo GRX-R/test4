ea6235dbeb5cdc61b91f6f18c1b146513d886ecb
GRX-R <grx1239@gmial.com>
2020-06-28 13:43:55 -0500

    Primeros Test de Inicio de sesión.

--- app/src/main/java/com/example/test4/MainActivity2.kt
+++ app/src/main/java/com/example/test4/MainActivity2.kt
@@ -1,5 +1,6 @@
 package com.example.test4
 
+import android.content.Intent
 import android.inputmethodservice.ExtractEditText
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -18,6 +19,7 @@ class MainActivity2 : AppCompatActivity() {
         val emailEText = findViewById<EditText>(R.id.emailcito) //SE REFERENCIA EL EMAIL
         val contraEText = findViewById<EditText>(R.id.editTextTextPassword2)
         val botoncito = findViewById<Button>(R.id.botonInicioS) //SE REFERENCIA EL BOTON INICIO
+        val permutarBot = findViewById<Button>(R.id.toggle1)
 
         //SE CREAN LAS VARIABLES DE PRUEBA DE COMPROBACIÓN
         val cuenta1 = "admin"
@@ -39,6 +41,12 @@ class MainActivity2 : AppCompatActivity() {
                 println("Error en el inicio de sesión...")
 
             }
+        }
+
+        permutarBot.setOnClickListener(){
+
+            val intento1 = Intent(this, MainActivity::class.java)
+            startActivity(intento1)
 
         }
 
